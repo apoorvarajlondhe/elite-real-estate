7ae8485153aaa0cfe01c64e8071663b6a023b642
apoorvarajlondhe <apoorvarajlondhe15@gmail.com>
2024-01-03 23:19:01 +0530

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://apoorvarajlondhe.github.io/elite-real-estate/",
+  base: "/elite-real-estate/",
 });
