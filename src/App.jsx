726d2b3fc0a1dca3b2829504a0c9f79077f4dd95
apoorvarajlondhe <apoorvarajlondhe15@gmail.com>
2024-01-04 18:55:04 +0530

    added complete file names in the import path  in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
+import Header from "./components/Header/Header.jsx";
+import Hero from "./components/Hero/Hero.jsx";
 import "./App.css";
-import Companies from "./components/Companies/Companies";
-import Residencies from "./components/Residencies/Residencies";
-import Value from "./components/Value/Value";
+import Companies from "./components/Companies/Companies.jsx";
+import Residencies from "./components/Residencies/Residencies.jsx";
+import Value from "./components/Value/Value.jsx";
 import Contact from "./components/Contact/Contact";
-import GetStarted from "./components/GetStarted/GetStarted";
-import Footer from "./components/Footer/Footer";
+import GetStarted from "./components/GetStarted/GetStarted.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 function App() {
   return (
     <div className="App">
